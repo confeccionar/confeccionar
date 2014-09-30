719642c32e5bac0a678a0efe3768481bafd24608
confeccionar <pikeryuk@gmail.com>
2014-09-30 10:56:46 -0500

    :D

---
+++ informes/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+<title>Documento sin título</title>
+<link href="../css/normalize.css" rel="stylesheet">
+<link href="../css/basic.css"  rel="stylesheet">
+<link href='https://www.google.com/fonts#UsePlace:use/Collection:Open+Sans:400,300,700' rel='stylesheet'>
+</head>
+<body>
+	<header class="reset">
+    	<div class="logo display restaurar">
+        	<img src="../images/header/logo.jpg">
+        </div>
+        <div class="user display">
+        	<figure class="userimage display restaurar">
+        	</figure>
+        	<div class="userdate display restaurar">
+        		<p class="username">Fernando rodriguez</p>
+        		<p class="userinfo">Ultima vez 18 de julio de 2014 - 8:01pm</p>
+        	</div>
+        </div>
+        <div class="usermenu display restaurar float">
+        	
+        		<div class="display">
+        			<img src="../images/header/calendario.jpg">
+        			<a>planeador</a>
+        		</div>
+        		<div class="display">
+        			<img src="../images/header/calendario.jpg">
+        			<a>mensajes</a>
+        		</div>
+        		<div class="display">
+        			<img src="../images/header/calendario.jpg">
+        			<a>notificaciones</a>
+        		</div>
+        		<div class="display">
+        			<img src="../images/header/calendario.jpg">
+        			<a>administrador</a>
+        		</div>
+        		<div class="display float">
+        			<img src="../images/header/calendario.jpg">
+        			<a>cerrar session</a>
+        		</div> 
+        	      	
+        </div>
+    </header>
+    <nav>
+        <ul>
+            <li>
+                <a>Informes</a>
+            </li>
+            <li>
+                <a>Pedidos</a>
+            </li>
+            <li>
+                <a>Clientes</a>
+            </li>
+            <li>
+                <a>Mis facturas</a>
+                <div class="span">
+                    <span>25</span>
+                </div>
+            </li>
+            <li>
+                <a>Cartera</a>
+            </li>
+            <li>
+                <a>Contabilidad</a>
+            </li>
+            <li>
+                <a>Inventario</a>
+                <div  class="span">
+                    <span>7</span>
+                </div>
+            </li>
+            <li>
+                <a>Procesos</a>
+            </li>
+            <li>
+                <a>Proveedores</a>
+            </li>
+            <li>
+                <a>Oportunidades</a>
+            </li>
+            <li>
+                <a>Planeador</a>
+            </li>
+        </ul>
+    </nav>
+    <script>
+
+    </script>
+</body>
+</html>
